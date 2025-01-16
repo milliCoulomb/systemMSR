fd80fcb02f7eaf9b6147c8e15f32ba2cbfe19af8
milliCoulomb <mathis.caprais@cea.fr>
2025-01-16 15:26:39 +0100

    Add ThermoHydraulicsSolver class to handle thermo-hydraulic calculations with specified parameters and geometries

--- physics/thermo.py
+++ physics/thermo.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 from typing import List, Dict
 import numpy as np
 import logging
-from methods.fvm import FVM
-from scipy.sparse import diags
-from scipy.sparse.linalg import spsolve
+import methods.method as Method
+from utils.geometry import CoreGeometry, SecondaryLoopGeometry
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -25,3 +25,13 @@ class ThermoHydraulicsState:
     core_temperature: np.ndarray  # Temperature distribution [K] in the core
     secondary_temperature: np.ndarray  # Temperature distribution [K] in the secondary loop
     flow_rate: float         # Mass flow rate [kg/s]
+
+class ThermoHydraulicsSolver:
+    def __init__(self, th_params: ThermoHydraulicsParameters, method: Method, core_geom: CoreGeometry, secondary_geom: SecondaryLoopGeometry):
+        # Extract thermo-hydraulic parameters
+        self.params = th_params
+        self.core_geom = core_geom
+        self.secondary_geom = secondary_geom
+        self.method = method
+        self.n_cells_primary = self.core_geom.n_cells_core + self.core_geom.n_cells_exchanger
+        self.n_cells_secondary = self.secondary_geom.n_cells_first_loop + self.secondary_geom.n_cells_second_loop + self.secondary_geom.n_cells_exchanger
