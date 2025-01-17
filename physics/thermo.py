11abdc9e710c0a92bb01fe4aa122a89d12ff3621
milliCoulomb <mathis.caprais@cea.fr>
2025-01-17 13:30:19 +0100

    Refactor ThermoHydraulicsSolver to support primary and secondary loop methods, enhancing temperature operator construction and steady-state solution assembly.

--- physics/thermo.py
+++ physics/thermo.py
@@ -6,7 +6,7 @@ import numpy as np
 import logging
 import methods.method as Method
 from utils.geometry import CoreGeometry, SecondaryLoopGeometry
-from scipy.sparse import diags
+from scipy.sparse import diags, spmatrix
 from scipy.sparse import bmat
 import scipy.sparse.linalg as spla
 from utils.states import ThermoHydraulicsState, NeutronicsState
@@ -25,13 +25,15 @@ class ThermoHydraulicsParameters:
     heat_exchanger_coefficient: float  # [W/m^3-K]
     expansion_coefficient: float  # [1/K]
 
+
 class ThermoHydraulicsSolver:
     def __init__(
         self,
         th_params_primary: ThermoHydraulicsParameters,
         th_params_secondary: ThermoHydraulicsParameters,
-        method: Method,
+        method_primary: Method,
         core_geom: CoreGeometry,
+        method_secondary: Method,
         secondary_geom: SecondaryLoopGeometry,
     ):
         # Extract thermo-hydraulic parameters
@@ -39,7 +41,8 @@ class ThermoHydraulicsSolver:
         self.params_secondary = th_params_secondary
         self.core_geom = core_geom
         self.secondary_geom = secondary_geom
-        self.method = method
+        self.method_primary = method_primary
+        self.method_secondary = method_secondary
         self.n_cells_primary = (
             self.core_geom.n_cells_core + self.core_geom.n_cells_exchanger
         )
@@ -49,89 +52,189 @@ class ThermoHydraulicsSolver:
             + self.secondary_geom.n_cells_exchanger
         )
 
-    def assemble_matrix_static(self, th_state_primary: ThermoHydraulicsState, th_state_secondary: ThermoHydraulicsState):
+    def build_temperature_advection_operator(
+        self,
+        th_state: ThermoHydraulicsState,
+        periodic: bool = True,
+        primary: bool = True,
+    ):
+        """
+        Build the advection operator for the temperature field (mdot * cp * dT/dx)
+        """
+        # build the gradient operator
+        if primary:
+            grad = self.method_primary.build_grad(
+                th_state.flow_rate
+                * self.params_primary.cp
+                * np.ones(self.n_cells_primary),
+                periodic=periodic,
+            )
+        else:
+            grad = self.method_secondary.build_grad(
+                th_state.flow_rate
+                * self.params_secondary.cp
+                * np.ones(self.n_cells_secondary),
+                periodic=periodic,
+            )
+        return grad
+
+    def build_temperature_diffusion_operator(
+        self,
+        th_state: ThermoHydraulicsState,
+        periodic: bool = True,
+        primary: bool = True,
+    ):
+        """
+        Build the diffusion operator for the temperature field (-k * A * d^2T/dx^2)
+        """
+        # build the stiffness operator
+        if primary:
+            stif = self.method_primary.build_stif(
+                self.params_primary.k
+                * np.ones(self.n_cells_primary)
+                * self.core_geom.core_radius**2
+                * np.pi,
+                periodic=periodic,
+            )
+        else:
+            stif = self.method_secondary.build_stif(
+                self.params_secondary.k
+                * np.ones(self.n_cells_secondary)
+                * self.secondary_geom.loop_radius**2
+                * np.pi,
+                periodic=periodic,
+            )
+        return stif
+
+    def build_heat_exchanger_operator(
+        self,
+        primary: bool = True,
+    ):
+        """
+        Build the heat exchanger operator for the temperature field. (h * A)
+        """
+        if primary:
+            heat_exchange = (
+                np.concatenate(
+                    [
+                        np.zeros(self.core_geom.n_cells_core),
+                        np.ones(self.core_geom.n_cells_exchanger),
+                    ]
+                )
+                * self.params_primary.heat_exchanger_coefficient
+                * (self.core_geom.core_radius**2 * np.pi)
+            )
+            matrix = self.method_primary.build_mass(heat_exchange)
+        else:
+            heat_exchange = (
+                np.concatenate(
+                    [
+                        np.zeros(self.secondary_geom.n_cells_first_loop),
+                        np.ones(self.secondary_geom.n_cells_exchanger),
+                        np.zeros(self.secondary_geom.n_cells_second_loop),
+                    ]
+                )
+                * self.params_secondary.heat_exchanger_coefficient
+                * (self.secondary_geom.loop_radius**2 * np.pi)
+            )
+            matrix = self.method_secondary.build_mass(heat_exchange)
+        return matrix
+
+    def assemble_matrix_static(
+        self,
+        th_state_primary: ThermoHydraulicsState,
+        th_state_secondary: ThermoHydraulicsState,
+    ):
         """
         Assemble the matrix for the static case
         """
-        # advection for the primary loop
-        adv_primary = self.method.build_grad(
-            th_state_primary.flow_rate * self.params_primary.cp * np.ones(self.n_cells_primary)
+        # build the operators
+        diff_primary = self.build_temperature_diffusion_operator(
+            th_state_primary, primary=True, periodic=True
         )
-        # diffusion for the primary loop, careful to multiply by the area
-        diff_primary = self.method.build_stif(
-            self.params_primary.k
-            * np.ones(self.n_cells_primary)
-            * self.core_geom.core_radius**2
-            * np.pi
+        adv_primary = self.build_temperature_advection_operator(
+            th_state_primary, primary=True, periodic=True
         )
-        # heat exchanger term, mask because it only applies to the exchanger in the primary loop
-        heat_exchange_primary = (
-            np.concatenate(
-                [
-                    np.zeros(self.core_geom.n_cells_core),
-                    np.ones(self.core_geom.n_cells_exchanger),
-                ]
-            )
-            * self.params_primary.heat_exchanger_coefficient
-            * (self.core_geom.core_radius**2 * np.pi)
+        heat_exchange_primary = self.build_heat_exchanger_operator(primary=True)
+        diff_secondary = self.build_temperature_diffusion_operator(
+            th_state_secondary, primary=False, periodic=False
         )
-        # advection for the secondary loop
-        adv_secondary = self.method.build_grad(
-            th_state_secondary.flow_rate * self.params_secondary.cp * np.ones(self.n_cells_secondary)
+        adv_secondary = self.build_temperature_advection_operator(
+            th_state_secondary, primary=False, periodic=False
         )
-        # kill periodicity by removing the last element of the first line
-        adv_secondary[0, -1] = 0
-        # diffusion for the secondary loop
-        diff_secondary = self.method.build_stif(
-            self.params_secondary.k
-            * np.ones(self.n_cells_secondary)
-            * self.secondary_geom.loop_radius**2
-            * np.pi
-        )
-        # kill periodicity by removing the last element of the first line and the first element of the last line
-        diff_secondary[0, -1] = 0
-        diff_secondary[-1, 0] = 0
-        # heat exchanger term, mask because it only applies to the exchanger in the secondary loop, so after n_cells_first_loop and before n_cells_first_loop + n_cells_exchanger
-        heat_exchange_secondary = (
-            np.concatenate(
-                [
-                    np.zeros(self.secondary_geom.n_cells_first_loop),
-                    np.ones(self.secondary_geom.n_cells_exchanger),
-                    np.zeros(self.secondary_geom.n_cells_second_loop),
-                ]
-            )
-            * self.params_secondary.heat_exchanger_coefficient
-            * (self.secondary_geom.loop_radius**2 * np.pi)
-        )
-        # assemble the matrix
+        heat_exchange_secondary = self.build_heat_exchanger_operator(primary=False)
         LHS_blocks = [
-            [diff_primary + adv_primary + heat_exchange_primary, -heat_exchange_primary],
-            [-heat_exchange_secondary, diff_secondary + adv_secondary + heat_exchange_secondary],
+            [
+                diff_primary + adv_primary + heat_exchange_primary,
+                -heat_exchange_primary,
+            ],
+            [
+                -heat_exchange_secondary,
+                diff_secondary + adv_secondary + heat_exchange_secondary,
+            ],
         ]
+        # print sizes of the blocks
+        print(f"Size of diff_primary: {diff_primary.shape}")
+        print(f"Size of adv_primary: {adv_primary.shape}")
+        print(f"Size of heat_exchange_primary: {heat_exchange_primary.shape}")
+        print(f"Size of diff_secondary: {diff_secondary.shape}")
+        print(f"Size of adv_secondary: {adv_secondary.shape}")
+        print(f"Size of heat_exchange_secondary: {heat_exchange_secondary.shape}")
         LHS_mat = bmat(LHS_blocks)
         logger.debug("Matrix assembled for the static case.")
         return LHS_mat
-    
-    def solve_static(self, th_state_primary: ThermoHydraulicsState, th_state_secondary: ThermoHydraulicsState, neutronic_state: NeutronicsState):
+
+    def build_steady_state_rhs_vector(
+        self,
+        th_state_primary: ThermoHydraulicsState,
+        th_state_secondary: ThermoHydraulicsState,
+        neutronic_state: NeutronicsState,
+    ):
+        """
+        Build the right-hand side vector for the steady-state problem (Pv * A + )
+        """
+        # we need to impose an inhomogeneous boundary condition for the secondary loop (T(0) = T_in)
+        # therefore we need to modify the RHS vector so that the source term exactly cancels the term in the matrix
+        # CAREFUL THIS IS DISCRETIZATION DEPENDENT
+        boundary_condition = th_state_secondary.T_in * (
+            th_state_secondary.flow_rate
+            * self.params_secondary.cp
+            / self.secondary_geom.dx[0]
+            + self.params_secondary.k
+            / self.secondary_geom.dx[0] ** 2
+            * np.pi
+            * self.secondary_geom.loop_radius**2
+        )
+        source_term_secondary = np.concatenate(
+            [
+                np.array([boundary_condition]),
+                np.zeros(self.secondary_geom.n_cells_first_loop - 1),
+                np.zeros(self.secondary_geom.n_cells_exchanger),
+                np.zeros(self.secondary_geom.n_cells_second_loop),
+            ]
+        )
+        # CAREFUL POWER DENSITY IS ALREADY MULTIPLIED BY THE AREA
+        power_source = (
+            neutronic_state.power_density
+        )
+        RHS_vector = np.concatenate([power_source, source_term_secondary])
+        return RHS_vector
+
+    def solve_static(
+        self,
+        th_state_primary: ThermoHydraulicsState,
+        th_state_secondary: ThermoHydraulicsState,
+        neutronic_state: NeutronicsState,
+    ):
         """
         Solve the static thermo-hydraulic problem
         """
-        # assemble the matrix
         LHS = self.assemble_matrix_static(th_state_primary, th_state_secondary)
-        exchanger_primary = np.concatenate(
-            [np.zeros(self.core_geom.n_cells_core), np.ones(self.core_geom.n_cells_exchanger)]
-        ) * self.params_primary.heat_exchanger_coefficient * (self.core_geom.core_radius**2 * np.pi) * th_state_secondary.T_in
-        exchanger_secondary = np.concatenate(
-            [np.zeros(self.secondary_geom.n_cells_first_loop), np.ones(self.secondary_geom.n_cells_exchanger), np.zeros(self.secondary_geom.n_cells_second_loop)]
-        ) * self.params_secondary.heat_exchanger_coefficient * (self.secondary_geom.loop_radius**2 * np.pi) * th_state_secondary.T_in
-        rhs_vector = np.concatenate(
-            [neutronic_state.power_density + exchanger_primary, -exchanger_secondary]
+        rhs_vector = self.build_steady_state_rhs_vector(
+            th_state_primary, th_state_secondary, neutronic_state
         )
-        # solve the linear system
         T = spla.spsolve(LHS, rhs_vector)
         logger.debug("Static thermo-hydraulic problem solved.")
-        th_state_primary.temperature = T[:self.n_cells_primary]
-        th_state_secondary.temperature = T[self.n_cells_primary:]
+        th_state_primary.temperature = T[: self.n_cells_primary]
+        th_state_secondary.temperature = T[self.n_cells_primary :]
         return th_state_primary, th_state_secondary
-
-
