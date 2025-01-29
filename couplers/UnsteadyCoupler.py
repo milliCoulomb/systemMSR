c7a19dddca678a4ae4adc93991175012a585dd5d
milliCoulomb <mathis.caprais@cea.fr>
2025-01-29 18:29:04 +0100

    Enhance UnsteadyCoupler: add turbulence handling in flow rate calculations for improved simulation accuracy

--- couplers/UnsteadyCoupler.py
+++ couplers/UnsteadyCoupler.py
@@ -8,6 +8,7 @@ from utils.geometry import CoreGeometry, SecondaryLoopGeometry
 from utils.states import ThermoHydraulicsState, NeutronicsState
 from physics.thermo import ThermoHydraulicsSolver
 from physics.neutronics import NeutronicsSolver
+from physics.turbulence import turbulence_process
 from utils.time_parameters import TimeParameters
 
 
@@ -29,6 +30,7 @@ class UnsteadyCoupler:
         initial_th_state_primary: ThermoHydraulicsState,
         initial_th_state_secondary: ThermoHydraulicsState,
         operational_parameters: TimeParameters,
+        turbulence: bool,
     ):
         self.th_solver = th_solver
         self.neutronics_solver = neutronics_solver
@@ -36,6 +38,7 @@ class UnsteadyCoupler:
         self.initial_th_state_primary = initial_th_state_primary
         self.initial_th_state_secondary = initial_th_state_secondary
         self.operational_parameters = operational_parameters
+        self.turbulence = turbulence
 
     def solve(
         self
@@ -62,12 +65,30 @@ class UnsteadyCoupler:
                 -((core_geom.x - beam_center) ** 2)
                 / (2 * beam_width ** 2)
             )
-
         for i in range(time_parameters.num_time_steps):
             # at each time step there is a while loop to couple the neutronics and thermohydraulics
             # set the initial values of mass flow rates and temperature for the primary and secondary loops
-            current_th_state_primary.flow_rate = time_parameters.pump_values_primary[i]
-            current_th_state_secondary.flow_rate = time_parameters.pump_values_secondary[i]
+            current_flow_rate = time_parameters.pump_values_primary[i] * np.ones_like(self.th_solver.core_geom.dx)
+            if self.turbulence:
+                if i == 0:
+                    old_perturbation = np.zeros_like(current_flow_rate)
+                else:
+                    old_perturbation = new_perturbation
+                new_perturbation = turbulence_process(
+                    flow_rate=current_flow_rate,
+                    old_perturbuation=old_perturbation,
+                    core_radius=self.th_solver.core_geom.core_radius,
+                    core_length=self.th_solver.core_geom.core_length,
+                    density=self.th_solver.params_primary.rho,
+                    viscosity=self.th_solver.params_primary.mu,
+                    dt=time_parameters.time_step,
+                    dx=self.th_solver.core_geom.dx[0],
+                )
+                # new perturbation does not have units, it is a relative perturbation
+                current_th_state_primary.flow_rate = current_flow_rate * (1 + new_perturbation)
+            else:
+                current_th_state_primary.flow_rate = current_flow_rate
+            current_th_state_secondary.flow_rate = time_parameters.pump_values_secondary[i] * np.ones_like(self.th_solver.secondary_geom.dx)
             current_th_state_secondary.T_in = (
                 time_parameters.secondary_inlet_temperature_values[i]
             )
